21f8fb82a2dd3acb34fac1f3faffd22a9a2f05aa
Bobby <s215818@dtu.dk>
2023-10-10 03:24:41 +0200

    Manual file handling and extention cleanup

--- extension.cpp
+++ extension.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
+#include <cstring>
 
 using namespace std;
 
 int main() {
-    // Run PowerShell to execute the 'code' CLI command
-    const char* command = "powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -Command \"code --list-extensions\" > vscode_extensions.txt";
+    fstream file;
+    
+    file.open("extensions.txt", ios::out);
 
-    // Execute the command
-    int result = system(command);
-
-    if (result == 0) {
-        cout << "List of VSCode extensions saved to vscode_extensions.txt" << std::endl;
-    } else {
-        cerr << "Error executing the command." << std::endl;
+    if (!file.is_open()) {
+        cerr << "Error opening file." << std::endl;
+        return 1;
     }
+
+    // Run PowerShell to execute the 'code' CLI command
+    const char* command = "powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -Command \"code --list-extensions\"";
+
+    FILE *pipe = _popen(command, "r");
+    if (!pipe) {
+        cerr << "Error opening pipe." << std::endl;
+        return 1;
+    }
+
+    char buffer[128];
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+        char *dotPosition = strchr(buffer, '.');
+        if (dotPosition) {
+            file << (dotPosition + 1);  // Write the text after the dot (excluding the dot)
+        } else {
+            file << buffer;  // Write the entire line if no dot is found
+        }
+    }
+
+    _pclose(pipe);
+
+    file.close();
+
+    cout << "List of VSCode extensions saved to extensions.txt" << endl;
 
     return 0;
 }
